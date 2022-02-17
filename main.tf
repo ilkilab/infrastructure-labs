9109a71eb72f36e44769bc150b2eba88270294e2
pierreilki <pvillard@ilki.fr>
2022-02-17 16:04:17 +0100

    export private ips

--- main.tf
+++ main.tf
@@ -28,12 +28,23 @@ resource "scaleway_instance_server" "server" {
   security_group_id= scaleway_instance_security_group.sg.id
 }
 
-output "ip_master" {
+output "public_ip_master" {
   value = scaleway_instance_server.server[0].public_ip
 }
-output "ip_worker1" {
+output "public_ip_worker1" {
   value = scaleway_instance_server.server[1].public_ip
 }
-output "ip_worker2" {
+output "public_ip_worker2" {
   value = scaleway_instance_server.server[2].public_ip
 }
+output "private_ip_master" {
+  value = scaleway_instance_server.server[0].private_ip
+}
+output "private_ip_worker1" {
+  value = scaleway_instance_server.server[1].private_ip
+}
+output "private_ip_worker2" {
+  value = scaleway_instance_server.server[2].private_ip
+}
+
+
