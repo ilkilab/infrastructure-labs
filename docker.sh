42982bd3668d122c9ba2254976f081df0eba0478
pierreilki <pvillard@ilki.fr>
2022-02-17 14:10:20 +0100

    add infra

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -yqq apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -yqq docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCl3ekSTD+NtyDerAsGoB0Sd4TgtjsHzFqtDWbdCFj6/yNFBbgjVdWzWncLH6uPYdWXljCL4nJdPqdPax6aklHgfBbF3X/zWf45xe4DdMVnkkC72L1iQqvkpr7KAjb5ppHA+1xBDIaRtvJ7LWBpKbl7Yu8Nn/e4jK87w3vWIkXJLXzj4cfAaaT0+yOxvi+CtFoZ6Dlryi7n48FfwM8twYwGmbu0KO9BM0ILIBlhUGxz5JEkRpcTHEoKMmo/dMMDHKW3alQ2a5UdzDVh8h5HpfwYPUh60R5yiJ83YkvCUq6WblKU4bm63wSHYwpm6C+tMHaq5Xh9lYkDP2ZThhIZw/AL9CHyTmjCcadcuUG9ItvMb08vNB/iHpdXunm0vLbxKZlKu4UtO3JbN+mErEKbep5CRQUWblZLakjQZtZqypsk/+wIgS3KQw35RuOAfE6URZh2naLLk2ex78KmQr3hR7EAN7bp8tumCl5NAV43OLPMTOA+89Kmi3maYY8Setgg7rs= root@ILKI-4CLG063" >> /root/.ssh/authorized_keys
--- main.tf
+++ main.tf
@@ -1,10 +1,32 @@
 resource "scaleway_instance_ip" "public_ip" {
-    count = 6
+    count = 3
 }
 
-resource "scaleway_instance_server" "web" {
-  count = 6
+resource "scaleway_instance_security_group" "sg" {
+  inbound_default_policy = "drop"
+  outbound_default_policy = "accept"
+}
+
+resource "scaleway_instance_server" "server" {
+  count = 3
+  name = "cka-${count.index}"
   type = "DEV1-S"
   image = "ubuntu_focal"
   ip_id = scaleway_instance_ip.public_ip[count.index].id
+  tags = [ "CKA", "LABS", "PIERRE" ]
+  user_data = {
+    docker        = "installed"
+    cloud-init = file("docker.sh")
+  }
+  security_group_id= scaleway_instance_security_group.sg.id
+}
+
+output "ip_master" {
+  value = scaleway_instance_server.server[0].public_ip
+}
+output "ip_worker1" {
+  value = scaleway_instance_server.server[1].public_ip
+}
+output "ip_worker2" {
+  value = scaleway_instance_server.server[2].public_ip
 }
