5619ba1760641811ab5b0a58d30355d3f518515b
pierreilki <pvillard@ilki.fr>
2022-02-17 11:08:35 +0100

    add count

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-resource "scaleway_instance_ip" "public_ip" {}
+resource "scaleway_instance_ip" "public_ip" {
+    count = 6
+}
 
 resource "scaleway_instance_server" "web" {
+  count = 6
   type = "DEV1-S"
   image = "ubuntu_focal"
-  ip_id = scaleway_instance_ip.public_ip.id
+  ip_id = scaleway_instance_ip.public_ip[count.index].id
 }
