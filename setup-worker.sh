113a5af229f5ec5dabdfd6459dfc0ab64912df68
pierre villard <pvillard@ilki.fr>
2022-04-25 17:49:26 +0200

    Update setup-worker.sh

--- setup-worker.sh
+++ setup-worker.sh
@@ -34,7 +34,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y kubelet=1.22.1-00 kubeadm=1.22.1-00 kubectl=1.22.1-00
+sudo apt-get install -y kubelet=1.23.0-00 kubeadm=1.23.0-00 kubectl=1.23.0-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 #
