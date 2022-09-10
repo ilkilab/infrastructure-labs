a635792ca38e8bf4cf74a4d45fc3a0f52d538e78
Adrien Huerre <32265063+ur2p0@users.noreply.github.com>
2022-09-10 11:25:19 +0200

    original main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "scaleway_instance_ip" "public_ip" {
-  count = 1
+  count = 3
 }
 
 resource "scaleway_instance_security_group" "sg" {
@@ -27,11 +27,40 @@ resource "scaleway_instance_server" "master" {
   security_group_id = scaleway_instance_security_group.sg.id
 }
 
+resource "scaleway_instance_server" "worker" {
+  count = 2
+  name  = "${var.name}-worker-${count.index}"
+  type  = "DEV1-S"
+  image = "ubuntu_focal"
+  ip_id = scaleway_instance_ip.public_ip[count.index].id
+  tags  = ["CKA", "LABS", "PIERRE"]
+  user_data = {
+    docker     = "installed"
+    cloud-init = file("setup-worker.sh")
+  }
+  private_network {
+    pn_id = scaleway_vpc_private_network.network.id
+  }
+  security_group_id = scaleway_instance_security_group.sg.id
+}
+
 output "public_ip_master" {
   value = scaleway_instance_server.master.public_ip
 }
-
+output "public_ip_worker1" {
+  value = scaleway_instance_server.worker[0].public_ip
+}
+output "public_ip_worker2" {
+  value = scaleway_instance_server.worker[1].public_ip
+}
 output "private_ip_master" {
   value = scaleway_instance_server.master.private_ip
 }
+output "private_ip_worker1" {
+  value = scaleway_instance_server.worker[0].private_ip
+}
+output "private_ip_worker2" {
+  value = scaleway_instance_server.worker[1].private_ip
+}
+
 
